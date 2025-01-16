e1c7efb56faf01a8398c0aa6ee1c1642a55168fa
Wian Gertenbach <67628074+WianGertenbach@users.noreply.github.com>
2025-01-16 18:25:26 +0200

    Code correctly scans for multiple occurences of substrings and adds them to the hash

--- sub-strings.rb
+++ sub-strings.rb
@@ -1,16 +1,18 @@
 def substrings(phrase,dictionary)
+  phrase = phrase.downcase
   dictionary.reduce(Hash.new(0)) do |result, word|
-    if phrase.include?(word)
-      result[word] += 1
-      result
-    else
-      result
+    matches = phrase.scan(word)
+    num_matches = matches.size
+
+    unless matches.empty?
+      result[word] += num_matches
     end
+    result
+
   end
-  
 end
 
-phrase = "howdy! backend development is fun! frontend development as well"
-dictionary = ["back", "end", "development", "front", "well", "how"]
+phrase = "howdy! backend development is fun! frontend development as well. How do you like it?"
+dictionary = ["back", "end", "development", "front", "well", "how", "i"]
 
 p substrings(phrase,dictionary)
